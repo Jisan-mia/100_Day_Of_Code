aff52bb012422db12eb8cf383bdb10f05e6aa15e
Jisan-Mia <jisanmia47@gmail.com>
2020-10-10 01:16:20 +0600

    problem solve of js fundamental

--- Day-17-Fundamental Javascript/function.js
+++ Day-17-Fundamental Javascript/function.js
@@ -19,3 +19,25 @@ function doubleIt(a) {
 var first = doubleIt(15);
 var second = doubleIt(10);
 console.log(first + second)
+
+//  functions to work on add, sub , mul and divide,  two number
+function add(n1, n2){
+    return n1 + n2;
+}
+function sub(n1, n2){
+    return n1 - n2;
+}
+function mul(n1, n2){
+    return n1 * n2;
+}
+function divide(n1, n2){
+    return n1 / n2;
+}
+var num1 =30;
+var num2 = 10;
+console.log(add(num1, num2));
+console.log(sub(num1, num2));
+console.log(mul(num1, num2));
+console.log(divide(num1, num2));
+
+
